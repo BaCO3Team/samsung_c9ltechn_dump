7f98009f31d27b744819744783e00ab417a9bec2
root <root@fv-az339-445.1gnjz1fn2jqeddhosk532c04te.jx.internal.cloudapp.net>
2022-12-17 16:55:10 +0000

    Add extras for c9ltezc-user 8.0.0 R16NW C9000ZCU1CSC2 release-keys
    Signed-off-by: root <root@fv-az339-445.1gnjz1fn2jqeddhosk532c04te.jx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## c9ltezc-user 8.0.0 R16NW C9000ZCU1CSC2 release-keys
+- Manufacturer: samsung
+- Platform: msm8952
+- Codename: c9ltechn
+- Brand: samsung
+- Flavor: c9ltezc-user
+- Release Version: 8.0.0
+- Id: R16NW
+- Incremental: C9000ZCU1CSC2
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 420
+- Fingerprint: samsung/c9ltezc/c9ltechn:8.0.0/R16NW/C9000ZCU1CSC2:user/release-keys
+- OTA version: 
+- Branch: c9ltezc-user-8.0.0-R16NW-C9000ZCU1CSC2-release-keys
+- Repo: samsung_c9ltechn_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/app/SBrowser_7.0/SBrowser_7.0.apk.* 2>/dev/null >> system/app/SBrowser_7.0/SBrowser_7.0.apk
+rm -f system/app/SBrowser_7.0/SBrowser_7.0.apk.* 2>/dev/null
+cat system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
+cat system/preload/Alipay/Alipay.apk.* 2>/dev/null >> system/preload/Alipay/Alipay.apk
+rm -f system/preload/Alipay/Alipay.apk.* 2>/dev/null
+cat system/preload/SHealth5/SHealth5.apk.* 2>/dev/null >> system/preload/SHealth5/SHealth5.apk
+rm -f system/preload/SHealth5/SHealth5.apk.* 2>/dev/null
+cat system/preload/WeChat/WeChat.apk.* 2>/dev/null >> system/preload/WeChat/WeChat.apk
+rm -f system/preload/WeChat/WeChat.apk.* 2>/dev/null
